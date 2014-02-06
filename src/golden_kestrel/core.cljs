1b69c2909ba4be99e4f86724223f7d886d6055fd
William Roe <willroe@gmail.com>
2014-02-06 17:51:38 +0000

    Bootstrap 2.3 whatever rubbish

--- src/golden_kestrel/core.cljs
+++ src/golden_kestrel/core.cljs
@@ -41,57 +41,77 @@
 
 (defn embed-form [data owner]
   (let [change-data (partial update-app data)]
-    (dom/div
-      nil
-      (dom/div #js {:className "form-control"}
-               (dom/label nil "Badge type")
-               (dom/select #js {:ref "badgeType"
-                                :value (:badge-type data)
-                                :defaultValue (:badge-type data)
-                                :onChange (partial change-data :badge-type)}
-                           (dom/option #js {:value ""} "Default")
-                           (dom/option #js {:value "1"} "1")
-                           (dom/option #js {:value "4"} "4")
-                           (dom/option #js {:value "donut"} "Donut")
-                           (dom/option #js {:value "medium-donut"} "Medium donut")
-                           (dom/option #js {:value "large-donut"} "Large donut")))
-      (dom/div #js {:className "form-control"}
-               (dom/label nil "Popover")
-               (dom/select #js {:ref "popover"
-                                :value (:popover data)
-                                :defaultValue (if (= "" (:badge-details data))
-                                                (:popover data)
-                                                "")
-                                :onChange (partial change-data :popover)}
-                           (dom/option #js {:value ""} "None")
-                           (dom/option #js {:value "top"} "Top")
-                           (dom/option #js {:value "right"} "Right")
-                           (dom/option #js {:value "bottom"} "Bottom")
-                           (dom/option #js {:value "left"} "Left")))
-      (dom/div #js {:className "form-control"}
-               (dom/label nil "Details")
-               (dom/select #js {:ref "details"
-                                :value (:badge-details data)
-                                :defaultValue (if (= "" (:popover data))
-                                                (:badge-details data)
-                                                "")
-                                :onChange (partial change-data :badge-details)}
-                           (dom/option #js {:value ""} "None")
-                           (dom/option #js {:value "right"} "Right")))
-      (dom/div #js {:className "form-control"}
-               (dom/label nil "Hide no mentions?")
-               (dom/input #js {:ref "hideNoMentions"
-                               :type "checkbox"
-                               :checked (:hide-no-mentions data)
-                               :onChange (partial change-data :hide-no-mentions)}
-                          nil))
-      (dom/div #js {:className "form-control"}
-               (dom/label nil "DOI")
-               (dom/input #js {:ref "doi"
-                               :type "text"
-                               :size "40"
-                               :defaultValue (:doi data)
-                               :onChange (partial change-data :doi)} nil)))))
+    (dom/form
+      #js {:className "form-horizontal"}
+      (dom/div
+        #js {:className "control-group"}
+        (dom/label #js {:className "control-label"}
+                   "Badge type")
+        (dom/div
+          #js {:className "controls"}
+          (dom/select #js {:ref "badgeType"
+                           :value (:badge-type data)
+                           :defaultValue (:badge-type data)
+                           :onChange (partial change-data :badge-type)}
+                      (dom/option #js {:value ""} "Default")
+                      (dom/option #js {:value "1"} "1")
+                      (dom/option #js {:value "4"} "4")
+                      (dom/option #js {:value "donut"} "Donut")
+                      (dom/option #js {:value "medium-donut"} "Medium donut")
+                      (dom/option #js {:value "large-donut"} "Large donut"))))
+      (dom/div
+        #js {:className "control-group"}
+        (dom/label #js {:className "control-label"}
+                   "Popover")
+        (dom/div
+          #js {:className "controls"}
+          (dom/select #js {:ref "popover"
+                           :value (:popover data)
+                           :defaultValue (if (= "" (:badge-details data))
+                                           (:popover data)
+                                           "")
+                           :onChange (partial change-data :popover)}
+                      (dom/option #js {:value ""} "None")
+                      (dom/option #js {:value "top"} "Top")
+                      (dom/option #js {:value "right"} "Right")
+                      (dom/option #js {:value "bottom"} "Bottom")
+                      (dom/option #js {:value "left"} "Left"))))
+      (dom/div
+        #js {:className "control-group"}
+        (dom/label #js {:className "control-label"}
+                   "Details")
+        (dom/div
+          #js {:className "controls"}
+          (dom/select #js {:ref "details"
+                           :value (:badge-details data)
+                           :defaultValue (if (= "" (:popover data))
+                                           (:badge-details data)
+                                           "")
+                           :onChange (partial change-data :badge-details)}
+                      (dom/option #js {:value ""} "None")
+                      (dom/option #js {:value "right"} "Right"))))
+      (dom/div
+        #js {:className "control-group"}
+        (dom/label #js {:className "control-label"}
+                   "Hide no mentions?")
+        (dom/div
+          #js {:className "controls"}
+          (dom/input #js {:ref "hideNoMentions"
+                          :type "checkbox"
+                          :checked (:hide-no-mentions data)
+                          :onChange (partial change-data :hide-no-mentions)}
+                     nil)))
+      (dom/div
+        #js {:className "control-group"}
+        (dom/label #js {:className "control-label"}
+                   "DOI")
+        (dom/div
+          #js {:className "controls"}
+          (dom/input #js {:ref "doi"
+                          :type "text"
+                          :size "40"
+                          :defaultValue (:doi data)
+                          :onChange (partial change-data :doi)} nil))))))
 
 (defn find-or-create-element
   [root class-name]
