eec59526ab65eefe90b65e6776d352fbd53f4f7b
William Roe <willroe@gmail.com>
2014-02-11 00:36:37 +0000

    Bump om version

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojurescript "0.0-2138"]
-                 [om "0.2.3"]
+                 [om "0.3.6"]
                  [com.facebook/react "0.8.0.1"]]
   :plugins [[lein-cljsbuild "1.0.1"]]
   :source-paths ["src"]
